72501d5131e91f1091d40ae9e6d681e3c7e7e7cb
Miguel Boland <mb1069@york.ac.uk>
2017-02-15 10:10:53 +0000

    Implemented scan transformation

--- implementation/ga.py
+++ implementation/ga.py
@@ -1,26 +1,30 @@
 import random
 import numpy
+from scanreader import Scan
 
 from deap import base
 from deap import creator
 from deap import tools
 from deap import algorithms
 
-NGEN = 500
+NGEN = 1
+POP = 100
 CXPB = 0.5
 MUTPB = 0.2
 
-MIN = 0
-MAX = 1
+MIN = -10
+MAX = 10
+
+scan = Scan("data/scan0")
 
 
 def randInt():
     return random.randint(0, 1)
 
 
-def evaluate(individual):
+def evaluate(individual, scan):
     block_vals = []
-    for val in xrange(0, IND_SIZE, 3):
+    for val in xrange(0, 3):
         s = sum(individual[val:val + 3])
         v = 0
         if s == 0:
@@ -32,11 +36,12 @@ def evaluate(individual):
         elif s == 3:
             v = 1.0
         block_vals.append(v)
+    print scan
     return sum(block_vals) / len(block_vals),
 
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
+creator.create("FitnessMin", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
 toolbox.register("attr_int", randInt)
@@ -44,11 +49,11 @@ toolbox.register("individual", tools.initRepeat, creator.Individual,
                  toolbox.attr_int, n=3)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.2)
+toolbox.register("mutate", tools.mutFlipBit, indpb=MUTPB)
 
 toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("mate", tools.cxUniform)
-toolbox.register("evaluate", evaluate)
+toolbox.register("mate", tools.cxUniform, indpb=CXPB)
+toolbox.register("evaluate", evaluate, scan=scan)
 
 stats = tools.Statistics(key=lambda ind: ind.fitness.values)
 stats.register("avg", numpy.mean, axis=0)
@@ -82,7 +87,7 @@ toolbox.decorate("mutate", check_bounds(MIN, MAX))
 
 
 def main():
-    pop = toolbox.population(n=300)
+    pop = toolbox.population(n=POP)
     hof = tools.HallOfFame(1)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", numpy.mean)
@@ -90,7 +95,7 @@ def main():
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
 
-    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=40,
+    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=CXPB, mutpb=MUTPB, ngen=NGEN,
                                    stats=stats, halloffame=hof, verbose=True)
 
 
